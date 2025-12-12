783b308fa3395deb32332d5c5b498b6ace8dea7c
Millimeter <akumakumu@gmail.com>
2025-12-13 06:42:41 +0700

    removing placeholders

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+	return (
+		<div className="min-h-svh grid place-items-center">
+			<div className="flex flex-col items-center gap-3">
+				<h1 className="text-4xl font-bold">lost?</h1>
+			</div>
+		</div>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 export default function Page() {
 	return (
-		<div className="flex flex-1 flex-col gap-4 p-4">
-			<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-				<div className="bg-muted/50 aspect-video rounded-xl" />
-				<div className="bg-muted/50 aspect-video rounded-xl" />
-				<div className="bg-muted/50 aspect-video rounded-xl" />
-			</div>
-			<div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
+		<div className="p-4 space-y-4">
+			<p className="text-4xl font-bold">Home</p>
 		</div>
 	);
 }

3c5116874a8858248c5cfac6d607d4c18dc5f15f
Millimeter <akumakumu@gmail.com>
2026-01-09 13:48:04 +0700

    add framer test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { motion } from "motion/react";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="p-8">
 			<Button>Click me</Button>
+			<motion.div
+				animate={{ rotate: 360 }}
+				transition={{ duration: 2, repeat: Infinity }}
+				className="w-20 h-20 bg-blue-500 mt-4"
+			/>
 		</div>
 	);
 }

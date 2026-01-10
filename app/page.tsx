26427320652627d0d993cf720359291781be2103
Millimeter <akumakumu@gmail.com>
2026-01-11 03:35:10 +0700

    cleaning up sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { motion } from "motion/react";
-
-export default function Home() {
+export default function Page() {
 	return (
-		<div className="p-8">
-			<Button>Click me</Button>
-			<motion.div
-				animate={{ rotate: 360 }}
-				transition={{ duration: 2, repeat: Infinity }}
-				className="w-20 h-20 bg-blue-500 mt-4"
-			/>
+		<div className="flex flex-1 flex-col gap-4 p-4">
+			<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+				<div className="bg-muted/50 aspect-video rounded-xl" />
+				<div className="bg-muted/50 aspect-video rounded-xl" />
+				<div className="bg-muted/50 aspect-video rounded-xl" />
+			</div>
+			<div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
 		</div>
 	);
 }
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import * as React from "react";
+import { LifeBuoy, Send, SquareTerminal } from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+} from "@/components/ui/sidebar";
+
+const data = {
+	user: {
+		name: "Yy",
+		email: "yy@wakimae.com",
+		avatar: "/avatars/shadcn.jpg",
+	},
+	navMain: [
+		{
+			title: "Home",
+			url: "#",
+			icon: SquareTerminal,
+		},
+	],
+	navSecondary: [
+		{
+			title: "Support",
+			url: "#",
+			icon: LifeBuoy,
+		},
+		{
+			title: "Feedback",
+			url: "#",
+			icon: Send,
+		},
+	],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	return (
+		<Sidebar
+			className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
+			{...props}
+		>
+			<SidebarContent>
+				<NavMain items={data.navMain} />
+				<NavSecondary items={data.navSecondary} className="mt-auto" />
+			</SidebarContent>
+			<SidebarFooter>
+				<NavUser user={data.user} />
+			</SidebarFooter>
+		</Sidebar>
+	);
+}
